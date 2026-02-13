a348166a8b235685d08bcecc23fa3ac02e08d7b4
Your Name <you@example.com>
2026-02-14 01:15:01 +0800

    add model filed

--- model/mysql/tenant_authority.go
+++ model/mysql/tenant_authority.go
@@ -12,14 +12,24 @@ type TenantAuthority struct {
 	AuthorityId     uint               `json:"authorityId" gorm:"not null;unique;primary_key;comment:角色ID;size:90"` // 角色ID
 	TenantID        uint               `json:"tenant_id" gorm:"column:tenant_id;type:int unsigned;not null" `       //租户ID
 	AuthorityName   string             `json:"authorityName" gorm:"comment:角色名"`                                    // 角色名
-	ParentId        uint               `json:"parentId" gorm:"comment:父角色ID"`                                       // 父角色ID
+	ParentId        *uint              `json:"parentId" gorm:"comment:父角色ID"`                                       // 父角色ID
 	DefaultRouter   string             `json:"defaultRouter" gorm:"comment:默认菜单;default:dashboard"`                 // 默认菜单(默认dashboard)
 	Children        []TenantAuthority  `json:"children" gorm:"-"`
-	AsBaseMenus     []TenantBaseMenu   `json:"menus" gorm:"many2many:tenant_authority_menus;foreignKey:authority_id;joinForeignKey:tenant_authority_authority_id;References:id;joinReferences:tenant_base_menu_id"`
+	TenantBaseMenus []TenantBaseMenu   `json:"menus" gorm:"many2many:tenant_authority_menus;foreignKey:authority_id;joinForeignKey:tenant_authority_authority_id;References:id;joinReferences:tenant_base_menu_id"`
 	Users           []TenantUser       `json:"-" gorm:"many2many:tenant_user_authority;foreignKey:authority_id;joinForeignKey:tenant_authority_authority_id;References:id;joinReferences:tenant_user_id"`
-	DataAuthorityId []*TenantAuthority `json:"dataAuthorityId" gorm:"many2many:tenant_data_authority_id;foreignKey:AuthorityId;joinForeignKey:tenant_authority_authority_id;References:AuthorityId;joinReferences:data_authority_id"`
+	DataAuthorityId []*TenantAuthority `json:"dataAuthorityId" gorm:"many2many:tenant_data_authority_id;foreignKey:AuthorityId;joinForeignKey:tenant_authority_authority_id;References:AuthorityId;joinReferences:data_authority_id_authority_id"`
 }
 
 func (TenantAuthority) TableName() string {
 	return "tenant_authorities"
+}
+
+type TenantDataAuthorityId struct {
+	TenantID                   uint `json:"tenant_id" gorm:"column:tenant_id;type:int unsigned;not null" ` //租户ID
+	TenantAuthorityAuthorityId uint `json:"tenant_authority_authority_id" gorm:"column:tenant_authority_authority_id;type:int unsigned;not null" `
+	DataAuthorityIdAuthorityId uint `json:"data_authority_id_authority_id" gorm:"column:data_authority_id_authority_id;type:int unsigned;not null" `
+}
+
+func (TenantDataAuthorityId) TableName() string {
+	return "tenant_data_authority_id"
 }
--- model/mysql/tenant_authority_btn.go
+++ model/mysql/tenant_authority_btn.go
@@ -4,5 +4,6 @@ type TenantAuthorityBtn struct {
 	AuthorityId         uint              `gorm:"comment:角色ID"`
 	TenantMenuID        uint              `gorm:"comment:菜单ID"`
 	TenantBaseMenuBtnID uint              `gorm:"comment:菜单按钮ID"`
+	TenantID            uint              `json:"tenant_id" gorm:"column:tenant_id;"` //租户ID
 	TenantBaseMenuBtn   TenantBaseMenuBtn ` gorm:"comment:按钮详情"`
 }

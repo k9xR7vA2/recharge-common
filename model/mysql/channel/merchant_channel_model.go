c54d760016c4a7e8fc9b31ac56421a9ad6307980
Your Name <you@example.com>
2026-02-14 11:56:28 +0800

    update constants

--- model/mysql/channel/merchant_channel_model.go
+++ model/mysql/channel/merchant_channel_model.go
@@ -1,6 +1,7 @@
 package channel
 
 import (
+	"github.com/shopspring/decimal"
 	constant "github.com/small-cat1/recharge-common/constant"
 	"gorm.io/datatypes"
 	"time"
@@ -12,11 +13,11 @@ type MerchantChannel struct {
 	ChannelType    constant.ChannelType          `gorm:"uniqueIndex:uk_tenant_merchant_channel;column:channel_type;type:tinyint;not null;comment:'通道类型: 1-基础通道 2-组合通道'" json:"channel_type"`                                           // 通道类型: 1-基础通道 2-组合通道
 	ChannelID      int                           `gorm:"uniqueIndex:uk_tenant_merchant_channel;column:channel_id;type:int;not null;comment:'通道ID/通道编码'" json:"channel_id"`                                                             // 通道ID/通道编码
 	ChannelCode    string                        `gorm:"index:idx_channel_code_merchant;column:channel_code;type:varchar(50);not null;comment:'通道编码(2xx)'" json:"channel_code"`                                                        // 通道编码(2xx)
-	SettlementRate float64                       `gorm:"column:settlement_rate;type:decimal(10,2);not null;default:0.00;comment:'结算费率'" json:"settlement_rate"`                                                                        // 结算费率
+	SettlementRate decimal.Decimal               `gorm:"column:settlement_rate;type:decimal(10,2);not null;default:0.00;comment:'结算费率'" json:"settlement_rate"`                                                                        // 结算费率
 	Amounts        datatypes.JSON                `gorm:"column:amounts;type:json;not null;comment:'金额'" json:"amounts"`                                                                                                                // 金额
 	Status         constant.TenantBusinessStatus `gorm:"index:idx_status;column:status;type:tinyint;not null;default:1;comment:'状态 1:启用 2:禁用,3未绑定'" json:"status"`                                                                     // 状态 1:启用 2:禁用,3未绑定
-	CreatedAt      time.Time                     `gorm:"column:created_at;type:datetime;default:null;default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt      time.Time                     `gorm:"column:updated_at;type:datetime;default:null;default:CURRENT_TIMESTAMP" json:"updated_at"`
+	CreatedAt      *time.Time                    `gorm:"column:created_at;type:datetime;default:null;default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt      *time.Time                    `gorm:"column:updated_at;type:datetime;default:null;default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
 
 // TableName get sql table name.获取数据库表名
--- model/mysql/product/supplier_products.go
+++ model/mysql/product/supplier_products.go
@@ -18,8 +18,8 @@ type SupplierProduct struct {
 	SettlementRate decimal.Decimal               `gorm:"column:settlement_rate;type:decimal(10,2) unsigned;not null;comment:'手续费'" json:"settlement_rate"` // 手续费
 	Amounts        datatypes.JSON                `gorm:"column:amounts;type:json;not null;comment:'开通金额,逗号分隔'" json:"amounts"`                             // 开通的金额配置
 	Status         constant.TenantBusinessStatus `gorm:"column:status;type:tinyint;not null;default:1;comment:状态 1:启用 2:禁用,3未绑定" json:"status"`
-	CreatedAt      time.Time                     `json:"created_at"`
-	UpdatedAt      time.Time                     `json:"updated_at"`
+	CreatedAt      *time.Time                    `json:"created_at"`
+	UpdatedAt      *time.Time                    `json:"updated_at"`
 
 	Tenant   tenant.Tenant    `gorm:"-;foreignKey:TenantID;references:TenantID" json:"tenant,omitempty"`
 	Supplier account.Supplier `gorm:"-;foreignKey:SupplierID;references:TenantID" json:"supplier,omitempty"`

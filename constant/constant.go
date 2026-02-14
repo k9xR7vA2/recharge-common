641896c0e17d5dbc0de849cd3449e69a420641b9
Your Name <you@example.com>
2026-02-14 11:36:09 +0800

    update constants

--- constant/constant.go
+++ constant/constant.go
@@ -2,6 +2,8 @@ package constant
 
 const (
 	SystemName         = "SaasAdmin"
+	USDTRate           = "usdt_rate"           //费率
+	RateExpireTime     = 5 * 60                //费率过期时间
 	GoogleVerifyAction = "view-wallet-address" // 谷歌验证器操作
 
 	PhoneRegexChinaStrict = `^1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}$`
@@ -36,4 +38,26 @@ const (
 	AccountRegRule = "[A-Za-z]+"
 	// RebateRegRule 费率验证规则
 	RebateRegRule = "^(0?\\.[1-9]\\d?|[1-9]\\d?(\\.\\d{1,2})?|100(\\.00?)?)$$"
+)
+
+const (
+	SupplierTotalOrder   = "supplier_total_order"
+	SupplierSuccessOrder = "supplier_success_order"
+	MerchantTotalOrder   = "merchant_total_order"
+	MerchantSuccessOrder = "merchant_success_order"
+
+	OrderTypeMerchant = 1 // 商户订单
+	OrderTypeSupplier = 2 // 供货商订单
+
+	EntityTypeAllMerchants    = "all_merchants"
+	EntityTypeMerchant        = "merchant"
+	EntityTypeMerchantChannel = "merchant_channel"
+	EntityTypeAllChannels     = "all_channels"
+	EntityTypeChannel         = "channel"
+
+	EntityTypeAllSuppliers    = "all_suppliers"
+	EntityTypeSupplier        = "supplier"
+	EntityTypeAllProducts     = "all_products"
+	EntityTypeProduct         = "product"
+	EntityTypeSupplierProduct = "supplier_product"
 )

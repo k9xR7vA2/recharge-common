505b8b5ac96228a90fd4c61d3268d645a1d1213b
Your Name <you@example.com>
2026-02-14 11:39:19 +0800

    update constants

---
+++ constant/credit_account_type.go
@@ -0,0 +1,10 @@
+package constant
+
+// CreditAccountHandleType 信用账户相关（仅供应商）
+type CreditAccountHandleType int
+
+const (
+	BusinessTypeCreditInit    CreditAccountHandleType = iota + 10 // 信用账户初始化
+	BusinessTypeCreditDeposit                                     // 信用账户充值 额度扣减(订单支付)
+	BusinessTypeCreditDeduct                                      // 信用账户扣减
+)

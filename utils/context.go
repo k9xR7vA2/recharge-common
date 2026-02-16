a3865fef0b29b68e30f89edbbca0195b5ab14f5b
Your Name <you@example.com>
2026-02-16 08:24:19 +0800

    update order model

--- utils/context.go
+++ utils/context.go
@@ -1,24 +1,25 @@
 package utils
 
 import (
-	"SaasApi/pkg/constant/constant"
 	"github.com/gin-gonic/gin"
+	"github.com/small-cat1/recharge-common/constant"
+	"github.com/small-cat1/recharge-common/model/mysql"
 )
 
 func ToSupplierContext(c *gin.Context) (*mysql.SupplierContext, bool) {
 	// 从原始 context 中获取 supplierContext
 	if sc, exists := c.Get(constant.SupplierContextKey); exists {
-		if supplierCtx, ok := sc.(*mysql2.SupplierContext); ok {
+		if supplierCtx, ok := sc.(*mysql.SupplierContext); ok {
 			return supplierCtx, true
 		}
 	}
 	return nil, false
 }
 
-func ToMerchantContext(c *gin.Context) (*mysql2.MerchantContext, bool) {
+func ToMerchantContext(c *gin.Context) (*mysql.MerchantContext, bool) {
 	// 从原始 context 中获取 MerchantContext
 	if mc, exists := c.Get(constant.MerchantContextKey); exists {
-		if merchantCtx, ok := mc.(*mysql2.MerchantContext); ok {
+		if merchantCtx, ok := mc.(*mysql.MerchantContext); ok {
 			return merchantCtx, true
 		}
 	}

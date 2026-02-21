13ccb9c3d9ffe035b3e3749ad0f697951f9db349
Your Name <you@example.com>
2026-02-21 21:48:21 +0800

    update channel model

--- model/mysql/channel_model.go
+++ model/mysql/channel_model.go
@@ -12,6 +12,7 @@ type Channel struct {
 	InterfaceID   uint                          `json:"interface_id" gorm:"column:interface_id;type:int unsigned;not null"`
 	ChannelCode   string                        `json:"channel_code" gorm:"column:channel_code;type:varchar(50);not null;uniqueIndex:uk_channel_code;comment:通道编码"`
 	ChannelName   string                        `json:"channel_name" gorm:"column:channel_name;type:varchar(100);not null;comment:通道名称"`
+	Lang          string                        `json:"lang" gorm:"column:lang;type:varchar(10);default:zh;comment:语言"`
 	BusinessType  constant.BusinessType         `json:"business_type" gorm:"column:business_type;type:varchar(100);not null;comment:业务类型"` // 业务类型：话费/游戏等
 	Status        constant.TenantBusinessStatus `json:"status" gorm:"column:status;type:tinyint;not null;default:1;comment:状态: 1-启用2-禁用"`
 	Payment       constant.PaymentType          `json:"payment" gorm:"column:payment;type:tinyint;not null;default:1;comment:支付方式"`

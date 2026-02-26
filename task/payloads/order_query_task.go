9b1d56d6ee3b443fe16bdcb5738ab06d1f376832
Your Name <you@example.com>
2026-02-26 16:41:57 +0800

    update query order task

--- task/payloads/order_query_task.go
+++ task/payloads/order_query_task.go
@@ -26,21 +26,25 @@ type OrderQueryTask struct {
 	SupplierOrderAmount   string    `json:"supplier_order_amount"`
 	SupplierOrderCreateAt time.Time `json:"supplier_order_create_at"`
 
-	// 三方查单信息
-	TradeNo         string `json:"trade_no"`   // 三方交易号
-	ChannelID       uint   `json:"channel_id"` // 通道ID，worker 用来查最新的接口配置
-	RechargeAccount string `json:"recharge_account"`
+	// 查单
+	InterfaceID     uint   `json:"interface_id"`      // 接口ID → 实时查配置
+	UpstreamOrderSn string `json:"upstream_order_sn"` // 三方网关订单号
 	Amount          string `json:"amount"`
-
-	// 查单策略（从 Interface 快照）
-	ApiSecret            string `json:"api_secret"`
-	ApiKey               string `json:"api_key"`
-	QueryUrl             string `json:"query_url"`
-	QueryInterval        int    `json:"query_interval"`         // 查单间隔（秒）
-	QueryMaxTimes        int    `json:"query_max_times"`        // 最大查单次数
-	PaySeconds           int    `json:"pay_seconds"`            // 支付有效期（秒）
-	OfficialSerialNumber string `json:"official_serial_number"` // 官方流水号
-	UpstreamOrderSn      string `json:"upstream_order_sn"`      //三方网关订单号
+	//// 三方查单信息
+	//TradeNo         string `json:"trade_no"`   // 三方交易号
+	//ChannelID       uint   `json:"channel_id"` // 通道ID，worker 用来查最新的接口配置
+	//RechargeAccount string `json:"recharge_account"`
+	//Amount          string `json:"amount"`
+	//
+	//// 查单策略（从 Interface 快照）
+	//ApiSecret            string `json:"api_secret"`
+	//ApiKey               string `json:"api_key"`
+	//QueryUrl             string `json:"query_url"`
+	//QueryInterval        int    `json:"query_interval"`         // 查单间隔（秒）
+	//QueryMaxTimes        int    `json:"query_max_times"`        // 最大查单次数
+	//PaySeconds           int    `json:"pay_seconds"`            // 支付有效期（秒）
+	//OfficialSerialNumber string `json:"official_serial_number"` // 官方流水号
+	//UpstreamOrderSn      string `json:"upstream_order_sn"`      //三方网关订单号
 
 	// 运行时
 	QueryCount int   `json:"query_count"` // 已查单次数

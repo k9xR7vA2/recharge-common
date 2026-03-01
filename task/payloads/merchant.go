1ceb61545925a9d99968bf7dee7eda1cbdb73822
Your Name <you@example.com>
2026-03-01 11:23:35 +0800

    update merchant paload task

--- task/payloads/merchant.go
+++ task/payloads/merchant.go
@@ -2,14 +2,11 @@ package payloads
 
 import (
 	"github.com/small-cat1/recharge-common/constant"
-	"time"
 )
 
 type MerchantOrderCallbackTask struct {
-	TenantID        uint                  `json:"tenant_id"`
-	OrderDate       time.Time             `json:"order_date"`
+	PayTime         int64                 `json:"pay_time"`
 	NotifyStatus    constant.NotifyStatus `json:"status"`
-	BusinessType    constant.BusinessType `json:"business_type"`
 	SystemOrderSn   string                `json:"system_order_sn"`
 	Amount          string                `json:"amount"`
 	NotifyUrl       string                `json:"notify_url"`

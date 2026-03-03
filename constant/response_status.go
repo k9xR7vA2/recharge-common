92857da2380e935102c3840f5291aa08f11a50a9
Your Name <you@example.com>
2026-03-03 17:19:11 +0800

    add response status

--- constant/response_status.go
+++ constant/response_status.go
@@ -3,9 +3,9 @@ package constant
 type GlobalResponseStatus string
 
 const (
-	ResponseStatusSuccess    NotifyStatus = "success"    //全局成功状态
-	ResponseStatusFail       NotifyStatus = "fail"       //全局失败状态
-	ResponseStatusProcessing NotifyStatus = "processing" //全局失败状态
+	ResponseStatusSuccess    GlobalResponseStatus = "success"    //全局成功状态
+	ResponseStatusFail       GlobalResponseStatus = "fail"       //全局失败状态
+	ResponseStatusProcessing GlobalResponseStatus = "processing" //全局失败状态
 )
 
 func (b GlobalResponseStatus) String() string {

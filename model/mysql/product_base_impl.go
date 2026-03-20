a06d6aa09a3d0c80cde714068816ccdbf6f6e700
Your Name <you@example.com>
2026-03-20 17:51:44 +0800

    update product model

--- model/mysql/product_base_impl.go
+++ model/mysql/product_base_impl.go
@@ -1,7 +1,6 @@
 package mysql
 
 import (
-	"encoding/json"
 	constant "github.com/k9xR7vA2/recharge-common/constant"
 	"gorm.io/datatypes"
 	"time"
@@ -14,11 +13,11 @@ type Product struct {
 	ProductName  string                        `json:"product_name" gorm:"column:product_name;type:varchar(100);not null;comment:产品名称"`
 	BusinessType constant.BusinessType         `json:"business_type" gorm:"column:business_type;type:varchar(100);not null;comment:业务类型"`
 	Status       constant.TenantBusinessStatus `json:"status" gorm:"column:status;type:tinyint;not null;default:1;comment:状态:1正常,2下架"`
-	Amount       datatypes.JSON                `json:"amount" gorm:"column:amount;type:json;not null;default:[];comment:金额"`
-	ValidTime    int                           `json:"valid_time" gorm:"column:valid_time;type:int;not null;default:1;comment:订单有效期"`
-	Attributes   datatypes.JSON                `json:"attributes" gorm:"column:attributes;type:json;not null;comment:业务属性"`
-	CreatedAt    time.Time                     `json:"created_at"`
-	UpdatedAt    time.Time                     `json:"updated_at"`
+	//Amount       datatypes.JSON                `json:"amount" gorm:"column:amount;type:json;not null;default:[];comment:金额"`
+	//ValidTime    int                           `json:"valid_time" gorm:"column:valid_time;type:int;not null;default:1;comment:订单有效期"`
+	Attributes datatypes.JSON `json:"attributes" gorm:"column:attributes;type:json;not null;comment:业务属性"`
+	CreatedAt  time.Time      `json:"created_at"`
+	UpdatedAt  time.Time      `json:"updated_at"`
 
 	TenantProducts []TenantProduct `json:"tenant_products,omitempty" gorm:"foreignKey:ProductID;references:ID"`
 	ProductSkus    []ProductSku    `json:"product_skus,omitempty" gorm:"foreignKey:ProductID;references:ID"`
@@ -48,12 +47,12 @@ func (g Product) GetStatus() constant.TenantBusinessStatus {
 	return g.Status
 }
 
-func (g Product) GetAmounts() []uint {
-	var amounts []uint
-	_ = json.Unmarshal(g.Amount, &amounts)
-	return amounts
-}
-
-func (g Product) GetValidTime() int {
-	return g.ValidTime
-}
+//func (g Product) GetAmounts() []uint {
+//	var amounts []uint
+//	_ = json.Unmarshal(g.Amount, &amounts)
+//	return amounts
+//}
+//
+//func (g Product) GetValidTime() int {
+//	return g.ValidTime
+//}

0c6a22dd21e193298dd00b254f78e742c0fd9495
Your Name <you@example.com>
2026-03-21 14:18:28 +0800

    update tenant channel model

--- model/mysql/tenant_channel.go
+++ model/mysql/tenant_channel.go
@@ -11,6 +11,7 @@ type TenantChannel struct {
 	BusinessType constant.BusinessType         `json:"business_type" gorm:"column:business_type;not null;comment:业务类型"`
 	TenantID     uint                          `json:"tenant_id" gorm:"column:tenant_id;not null;comment:租户ID"`
 	ChannelID    uint                          `json:"channel_id" gorm:"column:channel_id;not null;comment:通道ID"`
+	ChannelCode  string                        `json:"channel_code" gorm:"column:channel_code;type:varchar(50);not null;uniqueIndex:uk_channel_code;comment:通道编码"`
 	Payment      constant.PaymentType          `json:"payment" gorm:"column:payment;type:tinyint;not null;default:1;comment:支付方式"`
 	Status       constant.TenantBusinessStatus `json:"status" gorm:"column:status;not null;default:1;comment:状态 1:启用 2:禁用,3未绑定"` //1:启用 2:禁用,3未绑定
 	Remark       string                        `json:"remark" gorm:"column:remark;type:varchar(255);comment:备注说明"`

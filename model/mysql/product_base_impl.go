aab5e5f0e7fa9d660f1a2cbfed06a3e4ff4fe24f
Your Name <you@example.com>
2026-03-22 15:20:21 +0800

    update product mode

--- model/mysql/product_base_impl.go
+++ model/mysql/product_base_impl.go
@@ -8,15 +8,16 @@ import (
 
 // Product 实体实现
 type Product struct {
-	ID             uint                          `json:"id" gorm:"column:id;primaryKey;autoIncrement"`
-	ProductCode    string                        `json:"product_code" gorm:"column:product_code;unique;type:varchar(50);not null;comment:产品编码"`
-	ProductName    string                        `json:"product_name" gorm:"column:product_name;type:varchar(100);not null;comment:产品名称"`
-	BusinessType   constant.BusinessType         `json:"business_type" gorm:"column:business_type;type:varchar(100);not null;comment:业务类型"`
-	Status         constant.TenantBusinessStatus `json:"status" gorm:"column:status;type:tinyint;not null;default:1;comment:状态:1正常,2下架"`
-	IsDynamicPrice uint                          `json:"is_dynamic_price" gorm:"column:is_dynamic_price;type:tinyint;not null;default:1;comment:是否是动态金额:1是,2不是"`
-	Attributes     datatypes.JSON                `json:"attributes" gorm:"column:attributes;type:json;comment:业务属性"`
-	CreatedAt      time.Time                     `json:"created_at"`
-	UpdatedAt      time.Time                     `json:"updated_at"`
+	ID           uint                          `json:"id" gorm:"column:id;primaryKey;autoIncrement"`
+	ProductCode  string                        `json:"product_code" gorm:"column:product_code;unique;type:varchar(50);not null;comment:产品编码"`
+	ProductName  string                        `json:"product_name" gorm:"column:product_name;type:varchar(100);not null;comment:产品名称"`
+	BusinessType constant.BusinessType         `json:"business_type" gorm:"column:business_type;type:varchar(100);not null;comment:业务类型"`
+	Status       constant.TenantBusinessStatus `json:"status" gorm:"column:status;type:tinyint;not null;default:1;comment:状态:1正常,2下架"`
+	AmountType   uint                          `json:"amount_type" gorm:"not null;default:1;comment:金额类型: 1固定, 2区间, 3动态"`
+	Amount       datatypes.JSON                `json:"amount" gorm:"column:amount;type:json;not null;comment:金额"`
+	Attributes   datatypes.JSON                `json:"attributes" gorm:"column:attributes;type:json;comment:业务属性"`
+	CreatedAt    time.Time                     `json:"created_at"`
+	UpdatedAt    time.Time                     `json:"updated_at"`
 
 	TenantProducts []TenantProduct `json:"tenant_products,omitempty" gorm:"foreignKey:ProductID;references:ID"`
 	ProductSkus    []ProductSku    `json:"product_skus,omitempty" gorm:"foreignKey:ProductID;references:ID"`
--- model/mysql/tenant_product.go
+++ model/mysql/tenant_product.go
@@ -7,20 +7,19 @@ import (
 )
 
 type TenantProduct struct {
-	ID           uint                          `json:"id" gorm:"primaryKey"`
-	BusinessType constant.BusinessType         `json:"business_type" gorm:"not null;comment:业务类型"`
-	TenantID     uint                          `json:"tenant_id" gorm:"not null;comment:租户ID"`
-	ProductID    uint                          `json:"product_id" gorm:"not null;comment:产品ID"`
-	Status       constant.TenantBusinessStatus `json:"status" gorm:"not null;default:1;comment:状态 1:启用 2:禁用,3未绑定"` //1:启用 2:禁用,3未绑定
-	Amount       datatypes.JSON                `json:"amount" gorm:"column:amount;type:json;not null;comment:金额"`
-	AmountType   uint                          `json:"amount_type" gorm:"not null;default:1;comment:金额类型: 1固定, 2区间, 3动态"`
-	Remark       string                        `json:"remark" gorm:"size:255;comment:备注说明"`
-	CreatedAt    time.Time                     `json:"created_at"`
-	UpdatedAt    time.Time                     `json:"updated_at"`
+	ID              uint                          `json:"id" gorm:"primaryKey"`
+	BusinessType    constant.BusinessType         `json:"business_type" gorm:"not null;comment:业务类型"`
+	TenantID        uint                          `json:"tenant_id" gorm:"not null;comment:租户ID"`
+	ProductID       uint                          `json:"product_id" gorm:"not null;comment:产品ID"`
+	Status          constant.TenantBusinessStatus `json:"status" gorm:"not null;default:1;comment:状态 1:启用 2:禁用,3未绑定"`                //1:启用 2:禁用,3未绑定
+	SelectedAmounts datatypes.JSON                `json:"selected_amounts" gorm:"column:selected_amounts;type:json;comment:租户选中的金额"` // 改名
+	Remark          string                        `json:"remark" gorm:"size:255;comment:备注说明"`
+	CreatedAt       time.Time                     `json:"created_at"`
+	UpdatedAt       time.Time                     `json:"updated_at"`
 	// 关联字段
 	Tenant Tenant `gorm:"foreignKey:TenantID;references:TenantID" json:"tenant,omitempty"`
 
-	Product Product `gorm:"foreignKey:ID;references:ProductID" json:"product,omitempty"`
+	Product Product `gorm:"foreignKey:ProductID;references:ID" json:"product,omitempty"`
 }
 
 func (TenantProduct) TableName() string {

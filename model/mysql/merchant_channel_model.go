8fcb7c4e10d26699b8a6b22e3ac15cdf7a6d7928
Your Name <you@example.com>
2026-03-25 17:57:36 +0800

    update channel model

--- model/mysql/merchant_channel_model.go
+++ model/mysql/merchant_channel_model.go
@@ -1,14 +1,15 @@
 package mysql
 
 import (
-	"github.com/shopspring/decimal"
 	constant "github.com/k9xR7vA2/recharge-common/constant"
+	"github.com/shopspring/decimal"
 	"gorm.io/datatypes"
 	"time"
 )
 
 type MerchantChannel struct {
-	TenantID       uint                          `gorm:"uniqueIndex:uk_tenant_merchant_channel;index:idx_tenant_id;column:tenant_id;type:int unsigned;not null;comment:'租户ID'" json:"tenant_id"`                                       // 租户ID
+	TenantID       uint                          `gorm:"uniqueIndex:uk_tenant_merchant_channel;index:idx_tenant_id;column:tenant_id;type:int unsigned;not null;comment:'租户ID'" json:"tenant_id"` // 租户ID
+	BusinessType   constant.BusinessType         `json:"business_type" gorm:"column:business_type;type:varchar(100);not null;comment:业务类型"`
 	MerchantID     uint                          `gorm:"uniqueIndex:uk_tenant_merchant_channel;index:idx_merchant_id;index:idx_channel_code_merchant;column:merchant_id;type:int unsigned;not null;comment:'商户ID'" json:"merchant_id"` // 商户ID
 	ChannelType    constant.ChannelType          `gorm:"uniqueIndex:uk_tenant_merchant_channel;column:channel_type;type:tinyint;not null;comment:'通道类型: 1-基础通道 2-组合通道'" json:"channel_type"`                                           // 通道类型: 1-基础通道 2-组合通道
 	ChannelID      int                           `gorm:"uniqueIndex:uk_tenant_merchant_channel;column:channel_id;type:int;not null;comment:'通道ID/通道编码'" json:"channel_id"`                                                             // 通道ID/通道编码

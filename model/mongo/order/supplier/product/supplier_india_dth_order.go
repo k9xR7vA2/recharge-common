9264416e4fa19f5a0c9d5c1a850eba8b86a0557c
Your Name <you@example.com>
2026-03-30 17:59:54 +0800

    update supplier dth order model

--- model/mongo/order/supplier/product/supplier_india_dth_order.go
+++ model/mongo/order/supplier/product/supplier_india_dth_order.go
@@ -11,6 +11,9 @@ type SupplierIndiaDTHOrder struct {
 	supplier.SupplierOrderBase `bson:",inline"`
 	Operator                   constant.IndiaDthOperatorType `bson:"operator" json:"operator"`     // DTH 运营商
 	AccountID                  uint                          `bson:"account_id" json:"account_id"` // 来源账号ID，用于失败时释放额度
+	// 拆单关联字段
+	SourceOrderSn string `bson:"source_order_sn" json:"source_order_sn"` // 原始供货商单号，回调用这个
+	IsSplit       bool   `bson:"is_split" json:"is_split"`               // 是否拆单子单
 }
 
 func (o *SupplierIndiaDTHOrder) GetAppID() string { return o.AppID }

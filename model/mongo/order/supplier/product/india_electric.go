7579fc6300d024c4594442dedc4398f833a0fc27
Your Name <you@example.com>
2026-04-13 15:20:58 +0800

    add SupplierIndiaElectricOrder

---
+++ model/mongo/order/supplier/product/india_electric.go
@@ -0,0 +1,25 @@
+package product
+
+import (
+	"github.com/k9xR7vA2/recharge-common/constant"
+	"github.com/k9xR7vA2/recharge-common/model/mongo/order/supplier"
+)
+
+// SupplierIndiaElectricOrder  印度DTH订单
+// DTH 与话费差异：无充值速度、无携号转网，运营商为 DTH 运营商
+type SupplierIndiaElectricOrder struct {
+	supplier.SupplierOrderBase `bson:",inline"`
+	OperatorID                 uint `bson:"operator_id" json:"operator_id"` // DTH 运营商
+	AccountID                  uint `bson:"account_id" json:"account_id"`   // 来源账号ID，用于失败时释放额度
+	// 拆单关联字段
+	SourceOrderSn string `bson:"source_order_sn" json:"source_order_sn"` // 原始供货商单号，回调用这个
+	IsSplit       bool   `bson:"is_split" json:"is_split"`               // 是否拆单子单
+}
+
+func (o *SupplierIndiaElectricOrder) GetAppID() string { return o.AppID }
+func (o *SupplierIndiaElectricOrder) GetOperatorID() uint {
+	return o.OperatorID
+}
+func (o *SupplierIndiaElectricOrder) GetChargeSpeed() constant.ChargeSpeed {
+	return constant.Fast
+}

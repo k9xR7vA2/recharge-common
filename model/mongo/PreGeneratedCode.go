437cbee52c40fae1bcc5270e7079d5953c5994f3
Your Name <you@example.com>
2026-05-14 16:46:28 +0800

    update PreGeneratedCode model

--- model/mongo/PreGeneratedCode.go
+++ model/mongo/PreGeneratedCode.go
@@ -11,15 +11,19 @@ type PreGeneratedCode struct {
 	ProductCode   string              `bson:"product_code"`
 	Amount        string              `bson:"amount"`
 	Payment       string              `bson:"payment"`
-	PaymentMethod string              `bson:"payment_method"` // ✅ 修复：补上字段名和类型
-	AccountID     primitive.ObjectID  `bson:"account_id"`     // ✅ 新增：cookie 账号 ID
-	Account       string              `bson:"account"`        // ✅ 新增：账号（手机号）
-	CodeValue     string              `bson:"code_value"`     // 实际的码
-	PayUrl        string              `bson:"pay_url"`        // UPI 链接
-	Status        constant.CodeStatus `bson:"status"`         // available/used/expired
-	RefNo         string              `bson:"ref_no"`         // 三方流水号
-	ExpiredAt     int64               `bson:"expired_at"`     // 过期时间
+	PaymentMethod string              `bson:"payment_method"`
+	AccountID     primitive.ObjectID  `bson:"account_id"` // ：cookie 账号 ID
+	Account       string              `bson:"account"`    // ✅ 新增：账号（手机号）
+	CodeValue     string              `bson:"code_value"` // 实际的码
+	PayUrl        string              `bson:"pay_url"`    // UPI 链接
+	Status        constant.CodeStatus `bson:"status"`     // available/used/expired
+	RefNo         string              `bson:"ref_no"`     // 三方流水号
+	ExpiredAt     int64               `bson:"expired_at"` // 过期时间
 	UsedAt        int64               `bson:"used_at"`
 	CreatedAt     int64               `bson:"created_at"`
 	OrderSn       string              `bson:"order_sn"` // 使用时写入
+}
+
+func (PreGeneratedCode) CollName() string {
+	return "pre_generated_codes"
 }

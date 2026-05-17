ccdf698b04f17b20f1e04cd6cc48e5d16c03aae6
Your Name <you@example.com>
2026-05-17 11:56:25 +0800

    update PreCodeTask payloads

--- task/payloads/pre_code_task.go
+++ task/payloads/pre_code_task.go
@@ -4,7 +4,7 @@ package payloads
 type PreCodeTask struct {
 	TenantID          uint   `json:"tenant_id"`
 	ChannelCode       string `json:"channel_code"` // 租户通道编码
-	Amount            uint   `json:"amount"`
+	Amount            string `json:"amount"`
 	Count             int    `json:"count"`               // 需要产几张
 	TenantChannelCode string `json:"tenant_channel_code"` // 租户通道编码（冗余方便日志）
 }

156ddeebc2eae225f60761781a48975689cc5f74
Your Name <you@example.com>
2026-05-18 10:37:36 +0800

    update PreGeneratedCode model

--- model/mongo/PreGeneratedCode.go
+++ model/mongo/PreGeneratedCode.go
@@ -6,22 +6,24 @@ import (
 )
 
 type PreGeneratedCode struct {
-	ID            primitive.ObjectID  `bson:"_id"`
-	ChannelCode   string              `bson:"channel_code"`
-	ProductCode   string              `bson:"product_code"`
-	Amount        string              `bson:"amount"`
-	Payment       string              `bson:"payment"`
-	PaymentMethod string              `bson:"payment_method"`
-	AccountID     primitive.ObjectID  `bson:"account_id"` // ：cookie 账号 ID
-	Account       string              `bson:"account"`    // ✅ 新增：账号（手机号）
-	CodeValue     string              `bson:"code_value"` // 实际的码
-	PayUrl        string              `bson:"pay_url"`    // UPI 链接
-	Status        constant.CodeStatus `bson:"status"`     // available/used/expired
-	RefNo         string              `bson:"ref_no"`     // 三方流水号
-	ExpiredAt     int64               `bson:"expired_at"` // 过期时间
-	UsedAt        int64               `bson:"used_at"`
-	CreatedAt     int64               `bson:"created_at"`
-	OrderSn       string              `bson:"order_sn"` // 使用时写入
+	ID                   primitive.ObjectID  `bson:"_id"`
+	ChannelCode          string              `bson:"channel_code"`
+	ProductCode          string              `bson:"product_code"`
+	Amount               string              `bson:"amount"`
+	Payment              string              `bson:"payment"`
+	PaymentMethod        string              `bson:"payment_method"`
+	AccountID            primitive.ObjectID  `bson:"account_id"`             // ：cookie 账号 ID
+	Account              string              `bson:"account"`                // ✅ 新增：账号（手机号）
+	CodeValue            string              `bson:"code_value"`             // 实际的码
+	PayUrl               string              `bson:"pay_url"`                // UPI 链接
+	Status               constant.CodeStatus `bson:"status"`                 // available/used/expired
+	RefNo                string              `bson:"ref_no"`                 // 三方流水号
+	OfficialSerialNumber string              `bson:"official_serial_number"` //官方流水号
+	TransactionID        string              `bson:"transaction_id"`         //官方交易ID
+	ExpiredAt            int64               `bson:"expired_at"`             // 过期时间
+	UsedAt               int64               `bson:"used_at"`
+	CreatedAt            int64               `bson:"created_at"`
+	OrderSn              string              `bson:"order_sn"` // 使用时写入
 }
 
 func (PreGeneratedCode) CollName() string {
